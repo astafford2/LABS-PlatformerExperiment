eb7dd8beaba2b82652c373c29fd2b100575c5b0d
Andrew Stafford <astafford2@bsu.edu>
2021-01-13 17:17:52 -0500

    Add enemy gauge to testing level to test

--- project/src/Testing.gd
+++ project/src/Testing.gd
@@ -1,7 +1,29 @@
 extends Level
 
 
+var enemy_count := 0
+var total_enemy_count := 0
+onready var enemy_guage := $InfoHUDLayer/EnemyContainer/EnemyBar/HBoxContainer/Gauge
+
+
 func _ready():
 	player.enable_double_jump()
 	player.enable_melee()
 	player.enable_projectile()
+	
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		enemy_count += 1
+		total_enemy_count += 1
+
+
+func _process(_delta):
+	enemy_guage.value = (float(enemy_count) / float(total_enemy_count)) * 100
+
+
+func kill_enemy(body):
+	body.queue_free()
+	enemy_count -= 1
+
+
+func _on_Player_enemy_hit(body):
+	kill_enemy(body)
